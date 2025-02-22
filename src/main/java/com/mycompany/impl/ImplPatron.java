765a1ef7bc6bc55177870693b9ba02bafdda717c
Camilo Herrera <camilo.herrera.20021130@gmail.com>
2025-02-22 18:41:15 -0500

    impl

--- src/main/java/com/mycompany/impl/ImplPatron.java
+++ src/main/java/com/mycompany/impl/ImplPatron.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 //Brayan Camilo Herrera 160004711
-package com.mycompany.taller;
+package com.mycompany.impl;
 
 import com.mycompany.taller.interfaz.Menu;
 import java.sql.SQLException;
 import javax.swing.SwingUtilities;
 
-public class Taller {
+public class ImplPatron {
 
     public static void main(String[] args) throws SQLException {
         
